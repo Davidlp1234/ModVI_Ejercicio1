74f3f60265e0d8ff7c4c29171580486cc243b6e5
david leon peinado <davidlp1234@hotmail.com>
2018-09-22 16:13:31 -0300

    Add project files.

---
+++ ModVI_Ejercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModVI_Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
